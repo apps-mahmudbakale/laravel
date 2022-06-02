ba36a32ccdbb18c3487d6d821b7e2b8dc9d3f0bb
apps-mahmudbakale <apps.mahmud.bakale@gmail.com>
2022-06-02 13:21:44 +0100

    WIP

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,74 +1,65 @@
-@extends('layouts.auth')
+@extends('layouts.app')
 @section('title', 'Login')
 @section('content')
-    <div class="main d-flex justify-content-center w-100">
-        <main class="content d-flex p-0">
-            <div class="container d-flex flex-column">
-                <div class="row h-100">
-                    <div class="col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100">
-                        <div class="d-table-cell align-middle">
-
-                            <div class="text-center mt-4">
-                                <h1 class="h2">Welcome Back</h1>
-                                <p class="lead">
-                                    Sign in to your account to continue
-                                </p>
-                            </div>
-
-                            <div class="card">
-                                <div class="card-body">
-                                    <div class="m-sm-4">
-                                        <div class="text-center">
-                                            <img src="{{asset('img/favicon.png')}}"
-                                                class="img-fluid rounded-circle" width="132" height="132" />
-                                        </div>
-                                        <form method="POST" action="{{ route('login') }}">
-                                            @csrf
-                                            <div class="mb-3">
-                                                <label class="form-label">Email</label>
-                                                <input type="email" class="form-control form-control-lg @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"  autocomplete="email" autofocus
-                                                    placeholder="Enter your email" />
-
-                                                    @error('email')
-                                                    <span class="invalid-feedback" role="alert">
-                                                        <strong>{{ $message }}</strong>
-                                                     </span>
-                                                @enderror
-                                            </div>
-                                            <div class="mb-3">
-                                                <label class="form-label">Password</label>
-                                                <input id="password" type="password" class="form-control form-control-lg @error('password') is-invalid @enderror" name="password"  autocomplete="current-password" 
-                                                    placeholder="Enter your password" />
-
-                                                @error('password')
-                                                    <span class="invalid-feedback" role="alert">
-                                                        <strong>{{ $message }}</strong>
-                                                    </span>
-                                                @enderror
-                                                   
-                                                <small>
-                                                    <a href="pages-reset-password.html">Forgot password?</a>
-                                                </small>
-                                            </div>
-                                            <div>
-                                                <div class="form-check align-items-center">
-                                                    <input id="customControlInline" class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                                                    <label class="form-check-label text-small"
-                                                        for="customControlInline">Remember me next time</label>
-                                                </div>
-                                            </div>
-                                            <div class="text-center mt-3">
-                                                <button type="submit" class="btn btn-lg btn-primary">Sign in</button>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
+    <!-- Start Page Title -->
+    <div class="page-title-area" style="padding-top: 120px; padding-bottom:0px;">
+        <div class="d-table">
+            <div class="d-table-cell">
+                <div class="container">
+                    <h2>Login</h2>
                 </div>
             </div>
-        </main>
+        </div>
+
+        <div class="shape1"><img src="assets/img/shape1.png" alt="shape"></div>
+        <div class="shape2 rotateme"><img src="assets/img/shape2.svg" alt="shape"></div>
+        <div class="shape3"><img src="assets/img/shape3.svg" alt="shape"></div>
+        <div class="shape4"><img src="assets/img/shape4.svg" alt="shape"></div>
+        <div class="shape5"><img src="assets/img/shape5.png" alt="shape"></div>
+        <div class="shape6 rotateme"><img src="assets/img/shape4.svg" alt="shape"></div>
+        <div class="shape7"><img src="assets/img/shape4.svg" alt="shape"></div>
+        <div class="shape8 rotateme"><img src="assets/img/shape2.svg" alt="shape"></div>
     </div>
+    <!-- End Page Title -->
+
+    <!-- Start Login Area -->
+    <div class="ptb-80">
+        <div class="container">
+            <div class="auth-form">
+                <div class="auth-head">
+                    <a href="{{ route('login') }}">
+                        <img src="assets/img/favicon.png">
+                    </a>
+                    <p>Don't have an account yet? <a href="{{ route('register') }}">Sign Up</a></p>
+                </div>
+                <form action="{{ route('login') }}" method="POST">
+                    @csrf
+                    <div class="mb-3">
+                        <label class="form-label">E-mail</label>
+                        <input type="email" class="form-control @error('email') is-invalid @enderror" name="email"
+                            value="{{ old('email') }}">
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Password</label>
+                        <input type="password" class="form-control @error('password') is-invalid @enderror" name="password">
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <a href="">Forgot Password</a>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Login</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- End Login Area -->
 @endsection
